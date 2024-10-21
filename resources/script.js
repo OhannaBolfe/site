a1902bf50ee6ad2db9a0e1f70968580e6a6932a7
OhannaBolfe <ohanna.moon@gmail.com>
2024-10-21 12:45:54 -0300

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -33,7 +33,7 @@ const scrollToSection = (sectionId) => {
 document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear();
     const copyrightElement = document.querySelector('meta[name="copyright"]');
-    const yearElement = document.getElementById("currentYear");
+    const yearElement = document.getElementById("currentYear"); // Use o mesmo ID
 
     copyrightElement.setAttribute('content', `Ohanna Schmitt Bolfe, ${currentYear}`);
     yearElement.textContent = currentYear; 
