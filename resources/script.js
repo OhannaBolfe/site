91cf405316d463590566f87591695ae88561354e
OhannaBolfe <ohanna.moon@gmail.com>
2024-11-21 13:07:27 -0300

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -44,10 +44,12 @@ const accordionItems = document.querySelectorAll('.accordion-item');
 accordionItems.forEach(item => {
   const header = item.querySelector('.accordion-header');
   const content = item.querySelector('.accordion-content');
+  const accordionimg = document.querySelector('.profile-pic img');
 
   header.addEventListener('click', () => {
     // Alternar o estado do acordeão:
     content.classList.toggle('accordion-active');
     header.classList.toggle('accordion-active');
+    accordionimg.classList.toggle('accordion-img');
   });
 });
