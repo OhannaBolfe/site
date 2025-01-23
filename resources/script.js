6dd1e3cc02e0e11916fc1e5b76a00b6487c9bae9
OhannaBolfe <ohanna.moon@gmail.com>
2025-01-22 21:31:13 -0300

    Atualizar o script.js

--- resources/script.js
+++ resources/script.js
@@ -1,31 +1,11 @@
 $(document).ready(function() {
     // Fade out o loader após o carregamento completo da página
     window.onload = function() {
-        $('.loaderpage').fadeOut(500);
+        $('.loaderpage').fadeOut(1000);
     };
 });
 
-// Atualiza o ano no rodapé
 document.getElementById('currentYear').textContent = new Date().getFullYear();
-
-
-const toggleButton = document.querySelector('.toggle-button');
-const sidebar = document.querySelector('.sidebar');
-const mainContent = document.querySelector('.main-content');
-
-toggleButton.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-  mainContent.classList.toggle('active');
-  toggleButton.setAttribute('aria-expanded', sidebar.classList.contains('active'));
-});
-
-document.addEventListener('click', (event) => {
-  if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
-    sidebar.classList.remove('active');
-    mainContent.classList.remove('active');
-    toggleButton.setAttribute('aria-expanded', false);
-  }
-});
 
 document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear();
